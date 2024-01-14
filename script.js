1205699d9ffe313952671bd3af3e7d08ad96efb8
maleekberry95 <150798254+maleekberry95@users.noreply.github.com>
2024-01-14 08:21:48 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('Hello')
+var b =10;
+var c =30
+console.log(b)
+console.log(c)
+console.log(2345)
+const greeting = 'Hi GOOD AFTERNOON'
+console.log(greeting)
+console.log('5'+ '5')
+lert('Baytkay Travel')
+
